a251ea150bfefb2683a5a7f050ce3ff9be7d14a5
Hannibal-Woollen <55557867+Hannibal-Woollen@users.noreply.github.com>
2019-10-23 23:37:56 +0200

    Create 3d-plot.py

---
+++ 3d-plot.py
@@ -0,0 +1,16 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+x, y, z = np.meshgrid(np.arange(-1, 1, 0.3),
+                      np.arange(-1, 1, 0.3),
+                      np.arange(-1, 1, 0.7))
+
+u = (-(np.power(x, 2) + np.power(y, 2))*y)
+v = ((np.power(x,2) + np.power(y,2)*x))
+w = x * z
+ax.quiver(x, y, z, u, v, w, length=0.3)
+plt.show()
